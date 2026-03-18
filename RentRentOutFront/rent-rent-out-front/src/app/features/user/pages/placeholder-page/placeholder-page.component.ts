9d7bce3613baeb19a537b0efbeb9e6997772c5ed
micko112 <dimitrijemitic112@gmail.com>
2026-03-18 17:04:04 +0100

    Sidebar mockup

---
+++ RentRentOutFront/rent-rent-out-front/src/app/features/user/pages/placeholder-page/placeholder-page.component.ts
@@ -0,0 +1,27 @@
+﻿import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-placeholder-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './placeholder-page.component.html',
+  styleUrl: './placeholder-page.component.css'
+})
+export class PlaceholderPageComponent implements OnInit {
+  title = 'Stranica u pripremi';
+  description = 'Ova sekcija će uskoro biti dostupna.';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const data = this.route.snapshot.data || {};
+    if (data['title']) {
+      this.title = data['title'];
+    }
+    if (data['description']) {
+      this.description = data['description'];
+    }
+  }
+}
